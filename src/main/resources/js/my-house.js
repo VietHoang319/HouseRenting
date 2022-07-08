4aba67e95eb2d7fa791b01e848c96f756bbefc37
VietHoang319 <arishoang319@gmail.com>
2022-07-08 14:32:08 +0700

    sửa giao diện

--- src/main/resources/js/my-house.js
+++ src/main/resources/js/my-house.js
@@ -1,6 +1,6 @@
 function showMyHouse() {
     let content0 = $("#content0")
-    let str = `<div class="row"><button class="mt-4 mx-4 btn-nav" style="border: 2px solid black">Thêm nhà</button></div>
+    let str = `<div class="row"><button class="mt-4 btn-nav" style="border: 2px solid black">Thêm nhà</button></div>
                     <div class="row" id="list">
 
                     </div>`
