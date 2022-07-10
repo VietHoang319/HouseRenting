a1a8ed1ec22ebc65f3f9f6e59574e86c0b2ad151
vudinhson2001 <you@example.com>
2022-07-10 18:45:29 +0700

    10/7-sonvu-api findById image

--- src/main/java/com/example/houserenting/controller/ImageController.java
+++ src/main/java/com/example/houserenting/controller/ImageController.java
@@ -1,8 +1,12 @@
 package com.example.houserenting.controller;
 
+import com.example.houserenting.model.House;
 import com.example.houserenting.model.Image;
 import com.example.houserenting.service.impl.ImageServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
@@ -14,6 +18,7 @@ import javax.validation.Valid;
 import java.io.File;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Optional;
 
 @RestController
 @CrossOrigin("*")
@@ -32,4 +37,9 @@ public class ImageController {
         imageService.save(image);
         return new ResponseEntity<>(HttpStatus.CREATED);
     }
+    @GetMapping("/{id}")
+    public ResponseEntity<Optional<Image>> findCategoryId(@PathVariable Long id) {
+        return new ResponseEntity<>(imageService.findById(id),HttpStatus.OK);
+    }
+
 }
